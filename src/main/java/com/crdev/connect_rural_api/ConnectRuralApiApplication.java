cd9efb0c36fd364896334d9a060756c285a6b8a2
Israel-CR <israel.cruz@qrsof.com>
2025-11-25 23:50:12 -0600

    first commit

---
+++ src/main/java/com/crdev/connect_rural_api/ConnectRuralApiApplication.java
@@ -0,0 +1,13 @@
+package com.crdev.connect_rural_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectRuralApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectRuralApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crdev/connect_rural_api/ConnectRuralApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crdev.connect_rural_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectRuralApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
