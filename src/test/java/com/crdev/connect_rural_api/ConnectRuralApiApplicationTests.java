02867f47e7c8d95f6b6e98f33597886ee9b93123
Israel-CR <israel.cruz@qrsof.com>
2025-11-26 01:01:42 -0600

    add H2 database configuration for tests and update pom.xml to include H2 dependency

--- src/test/java/com/crdev/connect_rural_api/ConnectRuralApiApplicationTests.java
+++ src/test/java/com/crdev/connect_rural_api/ConnectRuralApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.crdev.connect_rural_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ConnectRuralApiApplicationTests {
 
 	@Test
